c3d5840a54d95a1c5bd86771c371e4dc4a5dc9d8
Rui Romer <ruiromer7a@gmail.com>
2025-03-03 11:01:23 -0300

    Getters e Setters de Pessoa e Produto

--- src/com/autopecas/entities/Pessoa.java
+++ src/com/autopecas/entities/Pessoa.java
@@ -1,4 +1,32 @@
 package com.autopecas.entities;
 
 public class Pessoa {
+
+    private String nome;
+    private String endereco;
+    private int cpf;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome (){
+        return nome;
+    }
+
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+
+    public String getEndereco(){
+        return endereco;
+    }
+
+    public void setCpf(int cpf){
+        this.cpf = cpf;
+    }
+
+    public int getCpf(){
+        return cpf;
+    }
 }
--- src/com/autopecas/entities/Produto.java
+++ src/com/autopecas/entities/Produto.java
@@ -1,4 +1,33 @@
 package com.autopecas.entities;
 
 public class Produto {
+
+    private String nome;
+    private int quantidade;
+    private double preco;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void setPreco(int preco){
+        this.preco = preco;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public int getQuantidade(){
+        return quantidade;
+    }
+
+    public double getPreco(){
+        return preco;
+    }
+
 }
